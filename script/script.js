873f16a7443e03920617cc6f7e1638b529ebcb06
go-max07 <johnmetry17@gmail.com>
2021-06-21 12:39:43 +0300

    made comment deletion for change in appearance of livepage

--- script/script.js
+++ script/script.js
@@ -72,7 +72,7 @@ $(document).ready(function() {
     var $form = $('form');
     $form.submit(function() {
         $.post($(this).attr('action'), $(this).serialize(), function(response) {
-            // do something here on success
+
         }, 'json');
         return false;
 
